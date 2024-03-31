9e1d461eca91313d7288ee70b0074ac51abca2c0
Dinar <sadin@ulaval.ca>
2024-03-31 19:28:44 -0400

    test pass BD

--- db_scripts/migrate_1.sql
+++ db_scripts/migrate_1.sql
@@ -1,23 +1,23 @@
 DROP TABLE IF EXISTS siner;
-create table band(
-    bandName varchar(50),
+CREATE TABLE band(
+    bandName VARCHAR(50),
     creation YEAR,
-    genre varchar(50),
+    genre VARCHAR(50),
     PRIMARY KEY (bandName)
 );
-insert into band (bandName, creation, genre) values
+INSERT INTO band (bandName, creation, genre) VALUES
 ('Crazy Duo', 2015, 'rock'),
 ('Luna', 2009, 'classical'),
 ('Mysterio', 2019, 'pop');
 
-alter table singer RENAME to musician;
-alter table musician CHANGE singerName musicianName varchar(50);
-alter table musician add column role varchar(50);
-alter table musician add column bandName varchar(50);
-update musician set role = 'vocals', bandName = 'Crazy Duo' where musicianName = 'Alina';
-update musician set role = 'guitar', bandName = 'Mysterio' where musicianName = 'Mysterio';
-update musician set role = 'percussion', bandName = 'Crazy Duo' where musicianName = 'Rainbow';
-update musician set role = 'piano', bandName = 'Luna' where musicianName = 'Luna';
+ALTER TABLE singer RENAME TO musician;
+ALTER TABLE musician CHANGE singerName musicianName VARCHAR(50);
+ALTER TABLE musician ADD COLUMN role VARCHAR(50);
+ALTER TABLE musician ADD COLUMN bandName VARCHAR(50);
+UPDATE musician SET role = 'vocals', bandName = 'Crazy Duo' WHERE musicianName = 'Alina';
+UPDATE musician SET role = 'guitar', bandName = 'Mysterio' WHERE musicianName = 'Mysterio';
+UPDATE musician SET role = 'percussion', bandName = 'Crazy Duo' WHERE musicianName = 'Rainbow';
+UPDATE musician SET role = 'piano', bandName = 'Luna' WHERE musicianName = 'Luna';
 
 
 
--- db_scripts/rollback_1.sql
+++ db_scripts/rollback_1.sql
@@ -1,5 +1,5 @@
-drop table if exists band;
-alter table musician drop column role;
-alter table musician drop column bandName;
-alter table musician CHANGE musicianName singerName varchar(50);
-alter table musician RENAME TO singer;
+DROP TABLE IF EXISTS band;
+ALTER TABLE musician DROP COLUMN role;
+ALTER TABLE musician DROP COLUMN bandName;
+ALTER TABLE musician CHANGE musicianName singerName VARCHAR(50);
+ALTER TABLE musician RENAME TO singer;
